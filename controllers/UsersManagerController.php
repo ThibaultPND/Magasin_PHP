50a2a51d78322ea78e7e0ce9d9c80f7735a50c72
Thibaut <126189643+ThibaultPND@users.noreply.github.com>
2024-03-21 00:36:45 +0100

    Création de la base du gestionnaire d'utilisateur

--- controllers/UsersManagerController.php
+++ controllers/UsersManagerController.php
@@ -3,6 +3,7 @@ class UsersManagerController {
     public function showUsersManagerl() {
         $pageTitle = 'Gestionnaire des utilisateurs';
         ob_start();
+        $style = "css/user_manager.css";
         include 'views/users_manager.php';
         $content = ob_get_clean();
         include 'views/layout.php';
--- views/users_manager.php
+++ views/users_manager.php
@@ -1,3 +1,5 @@
 <div class="userManager_container">
-    
+    <div class="search_container">
+        <h2>coucou</h2>
+    </div>
 </div>
